9f67acce487e0565ee741766718f495a6b862ded
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-04-21 11:55:20 +0300

    configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb-base'
+  ],
+  plugins: [],
+  rules: {
+    'generator-star-spacing': 'off',
+    'padded-blocks': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  }
+};
